ccb5c1cae7c526622616c1513e7beae7f4d4f186
Kens04 <kento090403@icloud.com>
2024-10-19 12:54:43 +0900

    first commit

---
+++ src/app/components/BBSCard.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+import { BBSData } from "@/app/types/types";
+
+interface BBSDataProps {
+  bbsData: BBSData;
+}
+
+const BBSCard = ({bbsData}: BBSDataProps) => {
+  const {id, username, title, content} = bbsData;
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{username}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        {content}
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Link href={`/bbs-posts/${id}`} className="text-blue-500">
+          Read More
+        </Link>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default BBSCard;
